d390f7082500a1b8e0be4dc8f4cd91f951368256
JaeSeungJae <valkyrie9923@gmail.com>
2024-11-06 12:44:20 +0900

    feat: 파일 읽어오는 함수 구현
    - readMeFile : md 확장자 파일을 읽어와서 이를 배열 단위로 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+
 class App {
-  async run() {}
+  async run() {
+    const productData = readMeFile('public/products.md');
+    console.log(productData);
+  }
+}
+
+function readMeFile(filePath) {
+  const data = fs.readFileSync(filePath, 'utf8');
+  const rows = data.trim().split('\n');
+  const headers = rows[0].split(',');
+  const jsonData = rows.slice(1).map(row => {
+    const values = row.split(',');
+    return headers.reduce((obj, header, index) => {
+      obj[header.trim()] = values[index].trim();
+      return obj;
+    }, {});
+  });
+
+  return jsonData;
 }
 
 export default App;
